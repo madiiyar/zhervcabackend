45954b28830194c3df40134a9ea6fc4e201db84f
Madiyar <amadiar654@gmail.com>
2025-06-01 20:15:42 +0500

    chaged connection

--- vc/Models/VcdbContext.cs
+++ vc/Models/VcdbContext.cs
@@ -47,7 +47,7 @@ public partial class VcdbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=vcdb;Username=postgres;Password=Madiyar777.");
+        => optionsBuilder.UseNpgsql("Host=vcdb.postgres.database.azure.com;Port=5432;Database=vcdb;Username=madiyar;Password=Admin777.;SSL Mode=Require;Trust Server Certificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
